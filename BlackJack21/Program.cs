392a862e08501ace0704d60878359a494cce1d1d
Alumno tarde <Alumno tarde@DESKTOP-KGTSG4J>
2023-10-25 18:56:17 +0100

    Agregar archivos de proyecto.

---
+++ BlackJack21/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+public class Cartas{
+    public enum PaloCartas
+    {
+        picas = 1,
+        corazones = 2,
+        trebol = 3,
+        rombos = 4
+    }
+    void CrearCarta()
+    {
+        Random rand = new Random();
+        int numero = rand.Next(1, 10);
+        var palo = Enum.GetValues(typeof(PaloCartas)).Cast<PaloCartas>().ToList();
+        PaloCartas randPaloCartas = palo[rand.Next(palo.Count)];
+    }
+}
+
