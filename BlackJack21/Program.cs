12bb0ca7507e5c83b860a7341c6ffbb76b7617f2
Alumno tarde <Alumno tarde@DESKTOP-KGTSG4J>
2023-10-30 17:19:29 +0000

    v2

--- BlackJack21/Program.cs
+++ BlackJack21/Program.cs
@@ -3,63 +3,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        Deck deck = new Deck();
-        deck.Shuffle();
+        Baraja baraja = new Baraja();
+        baraja.Barajar();
+        baraja.RobarCarta(1);
     }
 }
-public enum Suit //Enumerador para los palos de las cartas
+public enum Palo
 {
     Corazones,
     Diamantes,
     Rombos,
     Picas
 }
-public class Card //Clase para representar una carta con su palo y valor
-    {
-    public Suit Suit { get; }
-        public int Value { get; }
+public class Carta
+{
+    public Palo Palo { get; }
+    public int Valor { get; }
 
-        public Card(Suit suit, int value) //Constructor de clase
-        {
-            Suit = suit;
-            Value = value;
-        }
+    public Carta(Palo palo, int valor)
+    {
+        Palo = palo;
+        Valor = valor;
+    }
+    public void MostarCarta()
+    {
+        Console.WriteLine($"{Valor} de {Palo}");
     }
 
-    public class Deck
+}
+
+public class Baraja
+{
+    private List<Carta> cartas;
+    private Random rand;
+
+    public Baraja()
     {
-        private List<Card> cards;
-        private Random random;
+        cartas = new List<Carta>();
+        rand = new Random();
 
-        public Deck()
+        foreach (Palo palo in Enum.GetValues(typeof(Palo)))
         {
-            cards = new List<Card>();
-            random = new Random();
-
-            foreach (Suit suit in Enum.GetValues(typeof(Suit))) //Se crea la baraja en la lista "cards"
+            for (int valor = 1; valor <= 13; valor++)
             {
-                for (int value = 1; value <= 13; value++)
-                {
-                    cards.Add(new Card(suit, value));
-                    Console.WriteLine($"Carta: {value} de {suit}.");
-                }
-            }
-        }
-
-        public void Shuffle() //Método que mezcla la baraja
-        {
-            int n = cards.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = random.Next(n + 1);
-                Card card = cards[k];
-                cards[k] = cards[n];
-                cards[n] = card;
-                Console.WriteLine($"{card.Value} de {card.Suit}");
+                cartas.Add(new Carta(palo, valor));
+                //Console.WriteLine($"Carta: {valor} de {palo}.");
             }
         }
     }
+    public int ContarCartas()
+    {
+        return cartas.Count;
+    }
+    public void Barajar()
+    {
+        int n = ContarCartas();
+        while (n > 1)
+        {
+            n--;
+            int k = rand.Next(n + 1);
+            Carta Carta = cartas[k];
+            cartas[k] = cartas[n];
+            cartas[n] = Carta;
+            Console.WriteLine($"{Carta.Valor} de {Carta.Palo}");
+        }
+    }
+    public Carta RobarCarta(int contador)
+    {
+        Carta cartarobada = cartas[contador];
+        return cartarobada;
+    }
+}
 
 
 
